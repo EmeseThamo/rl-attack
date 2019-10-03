e2945c0c600d7f85f6ff68968e15e334b74d8c58
EmeseThamo <45718007+EmeseThamo@users.noreply.github.com>
2019-10-03 12:58:38 +0200

    Update __init__.py

--- rlattack/__init__.py
+++ rlattack/__init__.py
@@ -1,9 +1,9 @@
-% The __init__.py file used to be a required part of a package (old, pre-3.3 "regular package", not newer 3.3+ 
-% "namespace package").
-%
-% Python defines two types of packages, regular packages and namespace packages. Regular packages are traditional 
-% packages as they existed in Python 3.2 and earlier. A regular package is typically implemented as a directory 
-% containing an __init__.py file. When a regular package is imported, this __init__.py file is implicitly executed,
-% and the objects it defines are bound to names in the package’s namespace. The __init__.py file can contain the
-% same Python code that any other module can contain, and Python will add some additional attributes to the module
-% when it is imported.
+# The __init__.py file used to be a required part of a package (old, pre-3.3 "regular package", not newer 3.3+ 
+# "namespace package").
+#
+# Python defines two types of packages, regular packages and namespace packages. Regular packages are traditional 
+# packages as they existed in Python 3.2 and earlier. A regular package is typically implemented as a directory 
+# containing an __init__.py file. When a regular package is imported, this __init__.py file is implicitly executed,
+# and the objects it defines are bound to names in the package’s namespace. The __init__.py file can contain the
+# same Python code that any other module can contain, and Python will add some additional attributes to the module
+# when it is imported.
