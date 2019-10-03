a0e15f19cd5d6df0f04a31193e898616e2c3ba8d
EmeseThamo <45718007+EmeseThamo@users.noreply.github.com>
2019-10-03 13:00:10 +0200

    Added comments

--- rlattack/__init__.py
+++ rlattack/__init__.py
@@ -1,6 +1,7 @@
 # The __init__.py file used to be a required part of a package (old, pre-3.3 "regular package", not newer 3.3+ 
 # "namespace package").
 #
+#
 # Python defines two types of packages, regular packages and namespace packages. Regular packages are traditional 
 # packages as they existed in Python 3.2 and earlier. A regular package is typically implemented as a directory 
 # containing an __init__.py file. When a regular package is imported, this __init__.py file is implicitly executed,
